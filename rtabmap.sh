55a3a19df7ce9a68acff2b059afbe41ad832aaa7
ThaHypnotoad <asavchenkov1@gmail.com>
2017-01-14 17:02:05 -0800

    made rtabmap take the scanner as input to make grid map.

--- rtabmap.sh
+++ rtabmap.sh
@@ -3,4 +3,6 @@ roslaunch rtabmap_ros rgbd_mapping.launch rtabmap_args:="--delete_db_on_start" \
 frame_id:=frontCamera_link \
 rgb_topic:=$cameraSpace/rgb/image_rect_color \
 depth_registered_topic:=$cameraSpace/depth_registered/image_raw \
-camera_info_topic:=$cameraSpace/rgb/camera_info
+camera_info_topic:=$cameraSpace/rgb/camera_info \
+subscribe_scan:=true \
+scan_topic:=/frontScanner/scan
